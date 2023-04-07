79d31dbd6a6281a4692178b36d03ba8f6ea4cd94
SINEWORK NEGASI <105476205+Sineworknegasi@users.noreply.github.com>
2023-04-07 08:15:50 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 """0-minoperations module
 """
-def minOperations(n):
-    """Calculate the fewest number of operations needed to result in exactly n H characters in the file.
-
-    Args:
+  def minOperations(n):
+        """Calculate the fewest number of operations needed to result in exactly
+        
+        
+        Args:
         n (int): Number of H characters
-
-    Returns:
+        
+        Returns:
         int: Minimum number of operations needed
     """
     if n <= 1:
         # It's impossible to have fewer than 2 H characters
         return 0
-    # Initialize the number of operations and the number of H characters in the file
     min_ops = 0
     i = 2
 
     while i <= n:
         if n % i == 0:
-            # If n is a multiple of the current number of H characters, we can copy and paste
             min_ops += i
             n = n / i
         else:
